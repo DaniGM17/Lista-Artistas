1bc915043bb1b0cdb06f91fd752de0e55a1f03f1
DaniGM17 <daniagm75@gmail.com>
2022-11-23 18:58:35 -0600

    Ya puedo sobrescribir en un archivo

--- src/main/java/com/mycompany/listaartistas/Acciones.java
+++ src/main/java/com/mycompany/listaartistas/Acciones.java
@@ -4,10 +4,44 @@
  */
 package com.mycompany.listaartistas;
 
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  *
  * @author dania
  */
 public class Acciones {
-    
+     public static void escribirArchivo() throws IOException {
+        String archivo = "C:\\NetBeans\\artistas.txt";
+        FileWriter fw = new FileWriter(archivo, true);
+        /*FileWriter (archivo de archivo, agregado booleano): construye un objeto FileWriter
+        dado un objeto File. Si el segundo argumento es verdadero, los bytes se escribirán al 
+        final del archivo en lugar de al principio.
+        */
+        BufferedReader bufer = new BufferedReader(new InputStreamReader(System.in));
+        String entrada;
+        char respuesta;
+        try {
+                System.out.println("Escribe el artista: ");
+                entrada = bufer.readLine();
+                fw.write("\n"+entrada);//Añade la información al archivo
+        } catch (Exception e) {
+            System.out.println("Error al escribir en archivo");
+            e.printStackTrace();
+        } finally {
+            //Cerrrar el archivo si es que se pudo abrir para escritura
+            try {
+                //si se logró abrir el archivo, debemos cerrarlo
+                if (null != archivo) {
+                    fw.close();
+                }
+            } catch (Exception e2) {
+                System.out.println("Error al cerrar el archivo");
+                e2.printStackTrace();
+            }
+        }
+    }
 }
--- src/main/java/com/mycompany/listaartistas/Testing.java
+++ src/main/java/com/mycompany/listaartistas/Testing.java
@@ -5,11 +5,8 @@
 package com.mycompany.listaartistas;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
 
 /**
  *
@@ -17,44 +14,7 @@ import java.io.PrintWriter;
  */
 public class Testing {
 
-    public static void escribirArchivo() {
-        File archivo = null;
-        FileWriter archivoN = null;
-        FileReader fr = null;//para leer el archivo
-        PrintWriter pw = null;
-        BufferedReader bufer2 = new BufferedReader(new InputStreamReader(System.in));
-        String entrada;
-        char respuesta;
-        try {
-            archivo = new File("C:\\NetBeans\\artistas.txt");
-            fr = new FileReader(archivo);
-            pw = new PrintWriter(archivo);
-            do {
-                System.out.println("Escribe el artista: ");
-                entrada = bufer2.readLine();
-                pw.println(entrada);//Añade la información al archivp
-                System.out.println("Escribe n para parar");
-                entrada = bufer2.readLine();
-                respuesta = entrada.charAt(0);
-            } while (respuesta != 'n');
-        } catch (Exception e) {
-            System.out.println("Error al escribir en archivo");
-            e.printStackTrace();
-        } finally {
-            //Cerrrar el archivo si es que se pudo abrir para escritura
-            try {
-                //si se logró abrir el archivo, debemos cerrarlo
-                if (null != fr) {
-                    fr.close();
-                }
-            } catch (Exception e2) {
-                System.out.println("Errir al cerrar el archivo");
-                e2.printStackTrace();
-            }
-        }
-    }
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader buferTeclado = new BufferedReader(new InputStreamReader(System.in));
         String entrada;
         String fileName;
@@ -62,6 +22,6 @@ public class Testing {
         char seguir;
 
         System.out.println("Agregando artistas");
-        escribirArchivo();
+        Acciones.escribirArchivo();
     }
 }
